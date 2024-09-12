bc829a3b74b69fa5eee933814dde8678d4a5ce61
Aminu Lukmon Olamilekan <lukasaminu@gmail.com>
2024-09-12 14:05:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="card">
+      <Avatar />
+      <div className='data'>
+      <Intro />
+      <SkillList />
+      </div>
+      </div>
   );
+};
+
+function SkillList(props){
+  // console.log(props);
+  return (
+      <div className="skill-list" >
+        <Skill skill='HTML+CSS' emoji='💪' color="blue" />
+        <Skill skill='JavaScript' emoji='🤴' color="yellow" />
+        <Skill skill='Web Design' emoji='👼' color="#123456" />
+        <Skill skill='Git and GitHub' emoji='👍' color="orangered" />
+        <Skill skill='React' emoji='💪' color="skyblue" />
+        <Skill skill='Svelte' emoji='👲' color="red" />
+  </div>
+  );
+  
+};
+
+function Avatar(){
+  return  <img src="./avatar.jpeg" className="avatar" alt="Lukmon Aminu" />
+};
+
+function Intro(){
+  return (
+  <div>
+
+  <h1>Aminu Lukmon</h1>
+  <p>Full-stack web developer and teacher at Udemy. When not coding or preparing a course, I like to play board games, to cook (and eat), or to just enjoy the Portuguese sun at the beach</p>
+  </div>
+  )
 }
+
+function Skill(props){
+  // console.log(props);
+  return (
+      <div className="skill" style={{ backgroundColor: props.color}}>
+     <span>{props.skill}</span>
+     <span>{props.emoji}</span>
+  </div>
+  );
+  
+};
 
 export default App;
